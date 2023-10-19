b929ae8fd77555cbaab0c91263ea223198b7a961
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-10-19 17:06:31 +0600

    Create DecToBin.cpp

---
+++ DecToBin.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+
+
+void binform(int n){
+    bool start = false;
+    for(int i=31; i>=0; i--){
+        int bit = (n>>i)&1;
+        if(bit) start = true;
+        if(start) cout << bit ;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    binform(n);
+    return 0;
+}
